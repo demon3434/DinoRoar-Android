e552b3ef1c7c2d0597d30b53739185c3c9d74d89
demon3434 <411692310@qq.com>
2026-08-09 23:30:49 +0800

    修复关系人物管理的bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.dinoroar"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     signingConfigs {
